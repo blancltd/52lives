fd98fc71dfc466b8914435f59b081bfc3c5493b7
Edvinas Jurevicius <ikonitas@gmail.com>
2015-06-10 12:24:57 +0100

    Fix migrations

--- apps/lifes/migrations/0001_initial.py
+++ apps/lifes/migrations/0001_initial.py
@@ -8,9 +8,21 @@ import sorl.thumbnail.fields
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('pages', '0005_auto_20150529_1016'),
     ]
 
     operations = [
+        migrations.CreateModel(
+            name='LatestLifesBlock',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('number_of_lives', models.PositiveIntegerField(default=10)),
+                ('content_block', models.ForeignKey(editable=False, to='pages.ContentBlock', null=True)),
+            ],
+            options={
+                'verbose_name': 'Latest lifes',
+            },
+        ),
         migrations.CreateModel(
             name='Life',
             fields=[
@@ -20,14 +32,32 @@ class Migration(migrations.Migration):
                 ('last_name', models.CharField(max_length=30)),
                 ('number', models.PositiveIntegerField(null=True, verbose_name=b'Life number', blank=True)),
                 ('image', sorl.thumbnail.fields.ImageField(upload_to=b'uploads/lifes/life/%Y/%m/%d', verbose_name=b'Profile image', blank=True)),
+                ('email', models.EmailField(max_length=254, blank=True)),
+                ('home_phone', models.CharField(max_length=20, blank=True)),
+                ('mobile_phone', models.CharField(max_length=20, blank=True)),
                 ('content', models.TextField(verbose_name=b'Content about life')),
                 ('request', models.TextField(verbose_name=b'Life need')),
                 ('summary', models.TextField(verbose_name=b'What we did', blank=True)),
                 ('thank_you', models.TextField(verbose_name=b'Thank you content form life', blank=True)),
-                ('is_publish', models.BooleanField(default=False, verbose_name=b'Accept? Life number will be assign')),
+                ('is_published', models.BooleanField(default=False, verbose_name=b'Is accepted?')),
                 ('slug', models.SlugField()),
                 ('updated_at', models.DateTimeField(auto_now=True)),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
             ],
+            options={
+                'ordering': ('-number',),
+                'get_latest_by': 'id',
+            },
+        ),
+        migrations.CreateModel(
+            name='LifeBlock',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('content_block', models.ForeignKey(editable=False, to='pages.ContentBlock', null=True)),
+                ('life', models.ForeignKey(blank=True, to='lifes.Life', null=True)),
+            ],
+            options={
+                'verbose_name': 'Life',
+            },
         ),
     ]
--- apps/persons/migrations/0001_initial.py
+++ apps/persons/migrations/0001_initial.py
@@ -2,17 +2,34 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+import django.db.models.deletion
+import mptt.fields
+from django.conf import settings
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('lifes', '0005_auto_20150521_1057'),
+        ('lifes', '0001_initial'),
+        migrations.swappable_dependency(settings.BLANC_PAGES_MODEL),
+        ('pages', '0005_auto_20150529_1016'),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Nominator',
+            name='NominateFormBlock',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('recipient', models.EmailField(max_length=254)),
+                ('content_block', models.ForeignKey(editable=False, to='pages.ContentBlock', null=True)),
+                ('success_page', mptt.fields.TreeForeignKey(on_delete=django.db.models.deletion.SET_NULL, to=settings.BLANC_PAGES_MODEL, null=True)),
+            ],
+            options={
+                'abstract': False,
+            },
+        ),
+        migrations.CreateModel(
+            name='Person',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('title', models.CharField(max_length=10, choices=[(b'Mr', 'Mr'), (b'Mrs', 'Mrs'), (b'Ms', 'Ms'), (b'Miss', 'Miss'), (b'Dr', 'Dr'), (b'Sir', 'Sir')])),
