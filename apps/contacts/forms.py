4d6b4c2dfedc846b9b15961f69960b21e284ea7d
Alex Tomkins <tomkins@darkzone.net>
2015-07-15 15:42:29 +0100

    Remove dead code

--- apps/contacts/forms.py
+++ apps/contacts/forms.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from django import forms
-from django.core.mail import mail_managers
-from django.utils.translation import ugettext_lazy as _
 
 from .models import Contact
 
@@ -17,4 +15,3 @@ class ContactForm(forms.ModelForm):
             'subject': 'Your subject',
             'content': 'Your content',
         }
-
