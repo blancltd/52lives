68dc0c3fad8f01631ba629a1313bcc77fc81ccea
Steve Hawkes <steve@blanc.ltd.uk>
2015-07-16 00:38:00 +0100

    Fix errors - missing rename?

--- apps/lives/templatetags/lives_tags.py
+++ apps/lives/templatetags/lives_tags.py
@@ -4,10 +4,10 @@ from django import template
 
 register = template.Library()
 
-from lives.models import Live
+from lives.models import Life
 
 
 @register.assignment_tag
 def get_latest_lives(number):
-    return Live.objects.active()[:number]
+    return Life.objects.active()[:number]
 
