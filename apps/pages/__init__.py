5822d42052e29a51292d0e9b782095c30c874da1
Steve Hawkes <steve@blanc.ltd.uk>
2015-07-16 15:16:15 +0100

    Various CSS fixes, and homepage addition

--- apps/pages/__init__.py
+++ apps/pages/__init__.py
@@ -20,5 +20,20 @@ class DefaultLayout(blanc_pages.BlancPageLayout):
     }
 
 
-blanc_pages.register_template(DefaultLayout)
+class HomeLayout(blanc_pages.BlancPageLayout):
+    template_name = 'blanc_pages/homepage.html'
+    title = 'Default'
+    columns = {
+        'Intro': {
+            'width': 960,
+            'image_width': 960,
+        },
+        'Content': {
+            'width': 960,
+            'image_width': 960,
+        },
+    }
 
+
+blanc_pages.register_template(DefaultLayout)
+blanc_pages.register_template(HomeLayout)
