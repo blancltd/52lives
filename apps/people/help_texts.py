1dd7ef1f1c6607122ea0a1cb8d47196f51026e40
Steve Hawkes <steve@blanc.ltd.uk>
2015-11-12 12:05:47 +0000

    PEP8 the help_texts

--- apps/people/help_texts.py
+++ apps/people/help_texts.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 from django.utils.safestring import mark_safe
 
+
 PERSON_HELP_TEXTS = {
     'is_agreed': mark_safe(
         'Please ensure you have the permission of those involved before sharing personal details, '
-        'and familiarise yourself with our <a href="/terms/">Terms and conditions</a> and <a href="/privacy/">data protection policy</a>.'
+        'and familiarise yourself with our <a href="/terms/">Terms and conditions</a> and '
+        '<a href="/privacy/">data protection policy</a>.'
     )
 }
