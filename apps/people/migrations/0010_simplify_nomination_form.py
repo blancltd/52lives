7705e8c18433e158e4393e9b4985f36a84106546
Roman Goj <roman.goj@gmail.com>
2019-01-24 16:06:17 +0000

    Change migrations after moving to Python 2.7

--- apps/people/migrations/0010_simplify_nomination_form.py
+++ apps/people/migrations/0010_simplify_nomination_form.py
@@ -30,38 +30,8 @@ class Migration(migrations.Migration):
             field=models.ForeignKey(blank=True, to='countries.Country', null=True),
         ),
         migrations.AlterField(
-            model_name='nominee',
-            name='relation',
-            field=models.CharField(verbose_name='How do you know the nominee?', max_length=124, blank=True),
-        ),
-        migrations.AlterField(
-            model_name='nominee',
-            name='what_need',
-            field=models.TextField(verbose_name='What do they need?', blank=True),
-        ),
-        migrations.AlterField(
-            model_name='nominee',
-            name='why_help',
-            field=models.TextField(verbose_name='Why do they need help?', blank=True),
-        ),
-        migrations.AlterField(
-            model_name='person',
-            name='hear_about_us',
-            field=models.CharField(verbose_name='How did you hear about us?', max_length=120),
-        ),
-        migrations.AlterField(
-            model_name='person',
-            name='home_phone',
-            field=models.CharField(verbose_name='Phone', max_length=20, blank=True),
-        ),
-        migrations.AlterField(
             model_name='person',
             name='is_agreed',
-            field=models.BooleanField(default=False, help_text='Before submitting your nomination, please <a href="https://52lives.contentfiles.net/media/assets/file/Final_Privacy_Notice_Nominator_Q0KzKUJ.pdf">click here to read our privacy notice</a> and tick this box to confirm that you have read the policy notice and consent to the processing of your personal data and sensitive personal data.'),
-        ),
-        migrations.AlterField(
-            model_name='person',
-            name='title',
-            field=models.CharField(max_length=10, choices=[('Mr', 'Mr'), ('Mrs', 'Mrs'), ('Ms', 'Ms'), ('Miss', 'Miss'), ('Dr', 'Dr'), ('Sir', 'Sir')], blank=True),
+            field=models.BooleanField(default=False, help_text=b'Before submitting your nomination, please <a href="https://52lives.contentfiles.net/media/assets/file/Final_Privacy_Notice_Nominator_Q0KzKUJ.pdf">click here to read our privacy notice</a> and tick this box to confirm that you have read the policy notice and consent to the processing of your personal data and sensitive personal data.'),
         ),
     ]
