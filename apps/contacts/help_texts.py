b3350793c72515e6f946844db987396e4b8a0876
Roman Goj <roman.goj@gmail.com>
2019-02-08 11:03:08 +0000

    Use multiline string for legibility

--- apps/contacts/help_texts.py
+++ apps/contacts/help_texts.py
@@ -4,11 +4,9 @@ from django.utils.safestring import mark_safe
 
 CONTACT_HELP_TEXTS = {
     'is_agreed': mark_safe(
-        'Before submitting this form, please '
-        '<a href="/privacy/">'
-        'click here to read our privacy policy'
-        '</a> '
-        'and tick this box to confirm that you have read the policy notice and consent to the '
-        'processing of your personal data and sensitive personal data.'
+        """Before submitting this form, please
+           <a href="/privacy/">click here to read our privacy policy</a>
+           and tick this box to confirm that you have read the policy notice and consent to the 
+           processing of your personal data and sensitive personal data."""
     )
 }
