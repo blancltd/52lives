a0dcd67c1d28eaa1db871b80bbb448c829669271
Roman Goj <roman.goj@gmail.com>
2019-02-14 09:42:54 +0000

    Changle mobile menu button text

--- static/js/mobileMenu.js
+++ static/js/mobileMenu.js
@@ -1,20 +1,23 @@
 mobile_nav_button = document.querySelector('.nav__mobile-button');
+mobile_nav_label = document.querySelector('.nav__mobile-label')
 
-// Toggle aria-expanded for menu elements
-function updateMenuAria(element) {
+// Toggle aria-expanded for menu elements and change label
+function toggleMenu(element, label) {
     let aria_expanded = element.getAttribute('aria-expanded');
 
     if (aria_expanded === 'true') {
         element.setAttribute('aria-expanded', 'false');
+        label.innerHTML = 'Open Menu';
         aria_expanded = 'false';
     } else {
         element.setAttribute('aria-expanded', 'true');
+        label.innerHTML = 'Close Menu';
         aria_expanded = 'true';
     }
 }
 
 if (mobile_nav_button) {
     mobile_nav_button.addEventListener('click', function() {
-        updateMenuAria(mobile_nav_button);
+        toggleMenu(mobile_nav_button, mobile_nav_label);
     });
 }
