e8ca0601770627f3ab5c1bd5dc4d27586ab88109
Alex Tomkins <tomkins@darkzone.net>
2022-06-05 23:11:44 +0100

    Switch to redis for caching

--- lives52/settings/production.py
+++ lives52/settings/production.py
@@ -6,3 +6,7 @@ from .base import *  # NOQA @UnusedWildImport
 DEBUG = False
 
 TEMPLATE_DEBUG = DEBUG
+
+# Cache sessions for optimum performance
+if os.environ.get('REDIS_SERVERS'):
+    SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
